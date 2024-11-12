05c9d02efe4d84538ece550844dc35b8f5e92ffa
Mahfuz <aamahfuz.pro@gmail.com>
2024-11-13 01:28:41 +0600

    routes added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
-import router from "./app/routes/routes";
 
 const app: Application = express();
 
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { BookRoutes } from "../modules/Book/book.routes";
+import { BorrowRoutes } from "../modules/Borrow/borrow.routes";
+import { MemberRoutes } from "../modules/Member/member.routes";
+import { ReturnRoutes } from "../modules/Return/return.routes";
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: "/books",
+    route: BookRoutes,
+  },
+  {
+    path: "/members",
+    route: MemberRoutes,
+  },
+  {
+    path: "/borrow",
+    route: BorrowRoutes,
+  },
+  {
+    path: "/return",
+    route: ReturnRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
