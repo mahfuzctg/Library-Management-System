82fe15a3d8b2cff2fd19ed984575b00e91d4b8d0
Mahfuz <aamahfuz.pro@gmail.com>
2024-11-13 01:09:39 +0600

    app.ts file added

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes/routes";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    success: true,
+    message: "👋 Welcome to Library Management System! 😊",
+  });
+});
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
